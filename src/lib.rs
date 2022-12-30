c5386c9ec6b8435e19e89e45389354d8c9bf64ca
Nordgaren <nordgarentv@gmail.com>
2022-12-29 21:27:38 -0700

    removed unecessary print statements

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,7 @@ mod tests {
     fn read_c_string() {
         let hello_world: Vec<u8> = vec![0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(hello_world.as_slice()).unwrap();
-
         let string = br.read_cstr().unwrap();
-
         assert_eq!(string, "Hello, World!");
         assert_eq!(br.length - br.position(), hello_world.len() - br.position());
     }
@@ -24,9 +22,7 @@ mod tests {
     fn read_wide_string() {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
-
         let result = br.read_wcstr().unwrap();
-
         assert_eq!(result, "Hello, World!");
         assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
@@ -37,13 +33,13 @@ mod tests {
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let byte = br.read_u8().unwrap();
         assert_eq!(byte, 0x48);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
     fn read_u16() {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
-
         let ushort = br.read_u16().unwrap();
         assert_eq!(ushort, 0x48);
         assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
@@ -54,8 +50,9 @@ mod tests {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let uint = br.read_u32().unwrap();
-        println!("{:02x}",uint);
         assert_eq!(uint, 0x650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
+
     }
 
     #[test]
@@ -63,8 +60,8 @@ mod tests {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let ulong = br.read_u64().unwrap();
-        println!("{:02x}", ulong);
         assert_eq!(ulong, 0x6C006C00650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -72,8 +69,8 @@ mod tests {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let udouble_quad = br.read_u128().unwrap();
-        println!("{:02x}", udouble_quad);
         assert_eq!(udouble_quad, 0x570020002C006F006C006C00650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -82,15 +79,16 @@ mod tests {
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let sbyte = br.read_i8().unwrap();
         assert_eq!(sbyte, 0x48);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
     fn read_i16() {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
-
         let short = br.read_i16().unwrap();
         assert_eq!(short, 0x48);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -98,8 +96,8 @@ mod tests {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let int = br.read_i32().unwrap();
-        println!("{:02x}", int);
         assert_eq!(int, 0x650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -107,8 +105,8 @@ mod tests {
         let w_hello_world = vec![0x48, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x2c, 0x00, 0x20, 0x00, 0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 0x00, 0x21, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let long = br.read_i64().unwrap();
-        println!("{:02x}", long);
         assert_eq!(long, 0x6C006C00650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -118,6 +116,7 @@ mod tests {
         let double_quad = br.read_i128().unwrap();
         println!("{:02x}", double_quad);
         assert_eq!(double_quad, 0x570020002C006F006C006C00650048);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -126,6 +125,7 @@ mod tests {
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let float = br.read_f32().unwrap();
         assert_eq!(float, 9.275486e-39);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 
     #[test]
@@ -134,5 +134,6 @@ mod tests {
         let mut br = BinaryReader::new(w_hello_world.as_slice()).unwrap();
         let double = br.read_f64().unwrap();
         assert_eq!(double, 1.246114697985072e-306);
+        assert_eq!(br.length - br.position(), w_hello_world.len() - br.position());
     }
 }
